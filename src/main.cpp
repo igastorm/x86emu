0840447c98cdffbd85e01b49a89c03d8cd5e48a5
igastorm <167081895+igastorm@users.noreply.github.com>
2026-05-01 10:55:29 +0900

    Add allocation checks, noexcept and file handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <iostream>
+using std::basic_filebuf;
 using std::cout;
 
 // 1MB
@@ -27,20 +30,50 @@ private:
   // PC
   uint32_t eip;
 
+  // Init complete flag
+  bool ok;
+
 public:
-  Emulator(size_t, uint32_t, uint32_t);
-  ~Emulator(void);
+  Emulator(size_t, uint32_t, uint32_t) noexcept;
+  ~Emulator(void) noexcept;
+  bool is_ok(void) const;
 };
 
-Emulator::Emulator(size_t size, uint32_t eip, uint32_t esp)
-    : registers{}, eflags(0), memory(new uint8_t[size]), eip(eip) {
+Emulator::Emulator(size_t size, uint32_t eip, uint32_t esp) noexcept
+    : registers{}, eflags(0), memory(nullptr), eip(eip), ok(false) {
   this->registers[ESP] = esp;
+  if ((this->memory = new (std::malloc(size)) uint8_t) != nullptr) {
+    this->ok = true;
+  }
 }
 
-Emulator::~Emulator(void) { delete[] this->memory; }
+Emulator::~Emulator(void) noexcept {
+  std::free(this->memory);
+  this->memory = nullptr;
+}
+
+bool Emulator::is_ok(void) const { return this->ok; };
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: x86emu filename\n";
+    return 1;
+  }
+
   Emulator emulator(MEMORY_SIZE, 0x0000, 0x7c00);
+  if (!emulator.is_ok()) {
+    cout << "Failed to allocate emulator memory\n";
+    return 1;
+  }
+
+  basic_filebuf<char> fb;
+  if (fb.open(argv[1], std::ios::in | std::ios::out | std::ios::binary) ==
+      nullptr) {
+    std::cerr << "Can't open file: " << argv[1] << "\n";
+    return 1;
+  }
+
+  fb.close();
   cout << "Hello, x86emu!!\n";
   return 0;
 }
